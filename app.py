f9ed465fb51cb393fbd4361d7645e15f385978c9
alamsyah10 <alamsyah_imanudin0209@gmail.com>
2024-01-24 12:07:50 +0900

    add dashboard

--- app.py
+++ app.py
@@ -35,6 +35,10 @@ def callback():
 
     return 'OK'
 
+@app.route("/")
+def dashboard():
+    return render_template("dashboard.html")
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
